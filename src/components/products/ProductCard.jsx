a52f6785cf2748db20912b805cae4608bd2eb296
yjkdev21 <yjkdev21@gmail.com>
2025-09-20 18:37:17 +0900

    feat: 상품 품절 관리 시스템 구현

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { getThumbnailSrc } from "../../utils/image";
 import './ProductCard.css';
 
 const ProductCard = ({ product }) => {
+  const isSoldout = product.is_soldout;
+
+  const navigate = useNavigate();
   return (
-    <li className='product-card'>
-      <Link to={`/product/${product.pid}`}>
+    <li className={`product-card ${isSoldout ? 'soldout' : ''}`} onClick={() => navigate(`/product/${product.pid}`)}>
       <div className='photo'>
         <img src={getThumbnailSrc(product.thumbnail_url)} alt={product.name + " 썸네일"} />
       </div>
-        <p className='brand'>브랜드 {product.bid}</p>
-        <p className='name'>{product.name}</p>
-        <p className='price'>{product.price.toLocaleString('ko-KR')} 원</p>
-        <p className='mpoint'>최대 {product.point_rate}% M포인트</p>
-      </Link>
+      {product.brands && product.brands.bid ? (
+        <Link className='brand' to={`/brand/${product.brands.bid}`}>
+          {product.brands.name}
+        </Link>
+      )
+        : (
+
+          <p className='brand'>{product.sub_categories?.name || product.categories?.name || ''}</p>
+        )
+      }
+      <p className='name'>{product.name}</p>
+      <p className='price'>{product.price.toLocaleString('ko-KR')} 원</p>
+      <p className='mpoint'>최대 {product.point_rate}% M포인트</p>
+      {isSoldout && (
+        <div className='soldout-tag'>
+          <span>품절</span>
+        </div>
+      )}
     </li>
   );
 };
